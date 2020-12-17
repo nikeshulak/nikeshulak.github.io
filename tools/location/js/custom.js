88ea924d5980a37b34509154bcf8d208153ee2a9
Nikesh Ulak <nikesmartboy@gmail.com>
2020-12-17 20:35:40 +0545

    Location.

---
+++ tools/location/js/custom.js
@@ -0,0 +1,95 @@
+var url = "http://localhost/nikeshulak.github.io/tools/location";
+
+var locationName = "Unknown";
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+function myFunction() {
+  // fetch Location name from url first
+  lat = getUrlParameter('lat');
+  lng = getUrlParameter('lng');
+  locationName = getUrlParameter('locationName');
+  console.log(locationName)
+
+  if (lat == null) {
+    locationName = prompt("Location name", "Unknown");
+    console.log(locationName)
+    
+    if (locationName != null) {
+      // document.getElementById("demo").innerHTML = "Hello " + locationName + "! How are you today?";
+      // getLocation();
+
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else { 
+        // x.innerHTML = "Try enabling location."; // Geolocation is not supported by this browser. 
+      }
+    }
+  }
+  else {
+    console.log(locationName)
+
+    $("#map").html(`Copy link address and Share your location: 
+  <a href="${url}/?locationName=${locationName}&lat=${lat}&lng=${lng}">${locationName}</a> <br>
+
+  You are here:
+  <iframe 
+    width="100%" 
+    height="400" 
+    frameborder="0" 
+    scrolling="no" 
+    marginheight="0" 
+    marginwidth="0" 
+    src="https://maps.google.com/maps?q=${lat},${lng}&hl=es&z=14&amp;output=embed"
+   >
+   </iframe>
+   <p id="demo">Note: Please enable location and <a href="${url}">click here</a> to view or update your location.</p>
+   `);
+  }
+}
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+}
+
+function showPosition(position) {
+
+  var today = new Date();
+
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  var dateString = "Date: " + today + ", ";
+  var locationString = "/?locationName=" + locationName + "&lat=" + latitude + 
+  "&lng=" + longitude  
+  ;
+
+  // x.innerHTML = locationString;
+
+  // redirect to locationString
+  var actual_link = url + locationString;
+
+  console.log(actual_link)
+  // x.innerHTML = actual_link;
+  // x.innerHTML = 'Success';
+
+  window.location.href = actual_link;
+  // stored in cookie, but need to refresh inorder to get in php
+  // createCookie("location", locationString, "10"); 
+
+
+}
