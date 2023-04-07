986d17002af98d6615144f01190e4c753d27deaa
Nikesh Ulak <nikesh@fawesomeapps.com>
2023-04-07 20:58:29 +0545

    maha geeta upto 03.

---
+++ books/maha-geeta/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<style>
+		body {
+		    font-size: 18px;
+		    font-family: "Lora",serif;
+		    line-height: 30px;
+		    color: #8f8a86;
+		}
+	</style>
+</head>
+<body>
+Search: अष्टावक्र कहते
+
+<!-- <div class="vc_acf vc_txt_align_left -->
+
+<!-- https://oshoworld.com/maha-geeta-01/ -->
+<?php
+	for($i=11; $i<=91; $i++) {
+		echo 'curl -o '.$i.'.html https://oshoworld.com/maha-geeta-'.$i.'/<br />';
+	}
+
+	/*$file = isset($_GET['file']) ? $_GET['file'] : '01';
+
+	include('file/'.$file.'.html');*/
+?>
+
+</body>
+</html>
